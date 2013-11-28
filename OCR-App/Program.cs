a4ae49efa12574865388315aaca14c2ad00abeae
Robert Blomqvist <robert@meridium.se>
2013-11-28 14:03:58 +0100

    Added test image and test code to find text

--- OCR-App/Program.cs
+++ OCR-App/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using tesseract;
 
 namespace OCR_App
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+            var processor = new TesseractProcessor();
+
+            var succeed = processor.Init(@"D:\Tesseract-OCR\tessdata\", "eng", 3); // TesseractEngineMode: DEFAULT
+
+            if (succeed) {
+
+                processor.SetVariable("tessedit_pageseg_mode", "2"); // TesseractPageSegMode: PSM_SINGLE_LINE
+
+                using (var image = Image.FromFile(@"D:\GitHub\IV-OCR\images\pdtext_03.gif"))
+                {
+                    processor.Clear();
+                    processor.ClearAdaptiveClassifier();
+
+                    var result = processor.Apply(image);
+
+                    Console.WriteLine("Result: " + result);
+
+                    Console.WriteLine("Exit...");
+
+                }
+
+
+            }
+
         }
     }
 }
