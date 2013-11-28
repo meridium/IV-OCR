92dca6cc6c6b01b3ccc2cb29f89e641c54e2a0bb
Robert Blomqvist <robert@meridium.se>
2013-11-28 16:39:55 +0100

    ImageVault connection, downloading and processing files from specific vault

--- OCR-App/Program.cs
+++ OCR-App/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Net;
+using ImageVault.Client;
+using ImageVault.Client.Query;
+using ImageVault.Common.Data;
 
 namespace OCR_App
 {
@@ -8,39 +14,70 @@ namespace OCR_App
     {
         static void Main(string[] args) {
 
-            var sourceFile = @"D:\GitHub\IV-OCR\images\Golden_Globe_text_logo.png";
-            var targetFile = @"D:\GitHub\IV-OCR\images\temp_out.txt";
+            // Todo: extract to config
+            var sourceFile = @"D:\GitHub\IV-OCR\images\source.jpg";
+
             var resultString = "";
+            
+            var vaultId = 520;  // fedex vault on my local ImageVault instance
 
-            var p = new ProcessStartInfo();
+            // get images from vault and loop through
+            var client = ClientFactory.GetSdkClient();
 
-            p.CreateNoWindow = true;
-            p.WindowStyle = ProcessWindowStyle.Hidden;
+            var jpegWebFormat = new ImageFormat {MediaFormatOutputType = MediaFormatOutputTypes.Jpeg};
 
-            p.FileName = @"D:\Tesseract-OCR\tesseract.exe";
+            var mediaList = client.Query<MediaItem>().Where(m => m.VaultId == vaultId).ToList();
+            var ids = mediaList.Select(m => m.Id).ToList();
 
-            p.Arguments = string.Format("\"{0}\" \"{1}\" -l eng -psm 6", sourceFile, targetFile);
+            var webMedias = client.Query<WebMedia>().Where(wm => ids.Contains(wm.Id)).UseFormat(jpegWebFormat).ToList();
 
-            using (var ps = Process.Start(p)) {
-                // wait for command to finish
-                var result = ps.ExitCode;
-                if (result != 0) {
-                    return;
+            foreach (var webMedia in webMedias) {
+                var webclient = new WebClient();
+
+                webclient.DownloadFile(webMedia.Url, sourceFile);
+
+                var text = GetText(sourceFile);
+
+                Console.WriteLine("Text for image " + webMedia.Url + ": " + text);
+
+                Console.ReadLine();
+
+            }
+
+        }
+
+        private static string GetText(string source) {
+            var result = "";
+
+            const string targetFile = @"D:\GitHub\IV-OCR\images\temp_out.txt";
+            const string teserrectPath = @"D:\Tesseract-OCR\tesseract.exe";
+
+            for (int i = 0; i < 2; i++ )
+            {
+                var process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = teserrectPath,
+                        Arguments = string.Format("\"{0}\" \"{1}\" -l eng -psm {2}", source, targetFile, 5 * i + 1),
+                        UseShellExecute = true,
+                        CreateNoWindow = true,
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    }
+                };
+                process.Start();
+                process.WaitForExit();
+
+                // read the result from file
+                using (var f = File.OpenText(targetFile + ".txt"))
+                {
+                    result+=f.ReadToEnd();
                 }
-                ps.WaitForExit();
             }
 
-            // read the result from file
-            using (var f = File.OpenText(targetFile)) {
-                resultString = f.ReadToEnd();
-            }
+            File.Delete(targetFile + ".txt");
 
-            //File.Delete(sourceFile);
-            File.Delete(targetFile);
-
-            Console.WriteLine("Result: " + resultString);
-
-            Console.ReadLine();
+            return result;
         }
     }
 }
